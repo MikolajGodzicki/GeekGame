c89e05d011aad602352c7b46493177070ebc902f
Mikołaj Godzicki <mikolajgodzicki@gmail.com>
2023-11-01 17:18:31 +0100

    Updated camera movement

--- GeekKonkurs/Assets/Scripts/Camera/CameraController.cs
+++ GeekKonkurs/Assets/Scripts/Camera/CameraController.cs
@@ -4,52 +4,54 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Direction direction = Direction.Forward;
+    public float rotationSpeed = 90f;
+    public float smoothTime = 0.3f;
+    private float targetRotation = 0;
+    private Quaternion initialRotation;
+    private Quaternion targetQuaternion;
 
-    public void Update() {
-        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            SetDirection(-1);
-        }
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.RightArrow)) {
-            SetDirection(1);
-        }
+    private Dictionary<float, Direction> directions = new Dictionary<float, Direction>() {
+        { 0f, Direction.Forward },
+        { 90f, Direction.Left },
+        { 180f, Direction.Backward },
+        { 270f, Direction.Right },
+        { -90f, Direction.Right },
+        { -180f, Direction.Backward },
+        { -270f, Direction.Left },
+    };
 
-        switch (direction) {
-            case Direction.Forward:
-                transform.eulerAngles = new Vector3();
-                break;
-            case Direction.Right:
-                transform.eulerAngles = new Vector3(0f, 270f);
-                break;
-            case Direction.Backward:
-                transform.eulerAngles = new Vector3(0f, 180f);
-                break;
-            case Direction.Left:
-                transform.eulerAngles = new Vector3(0f, 90f);
-                break;
-        }
+    private Direction direction;
 
+    private void Start() {
+        initialRotation = transform.rotation;
+        targetQuaternion = initialRotation;
     }
 
-    private void SetDirection(int direction) {
-        if (this.direction + direction < 0) {
-            this.direction = Direction.Left;
-            return;
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            targetRotation += 90f;
+            if (targetRotation >= 360) {
+                targetRotation = 0;
+            }
         }
-        if (this.direction + direction > Direction.Max - 1) {
-            this.direction = Direction.Forward;
-            return;
+        else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            targetRotation -= 90f;
+            if (targetRotation <= -360) {
+                targetRotation = 0;
+            }
         }
 
-        this.direction += direction;
+        direction = directions[targetRotation];
+        Debug.Log(direction);
+
+        targetQuaternion = Quaternion.Euler(0, targetRotation, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetQuaternion, smoothTime * rotationSpeed * Time.deltaTime);
     }
 }
 
 public enum Direction {
     Forward,
-    Right,
     Backward,
     Left,
-
-    Max,
+    Right
 }
